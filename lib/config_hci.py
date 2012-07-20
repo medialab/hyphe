47a514fbc192790164c51b1afebd65b12bd29fdf
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2012-07-20 16:46:47 +0200

    fix config

---
+++ lib/config_hci.py
@@ -0,0 +1,12 @@
+import simplejson as json
+
+def load_config(filename = '../config.json') :
+    try :
+        with open(filename, 'r') as config_file:
+            return json.load(config_file)
+    except IOError as e:
+        print 'ERROR: Could not open config.json file : ', e
+        return False
+    except ValueError as e:
+        print 'ERROR: Config file is not valid JSON', e
+        return False
