bea8a70dfadc163226bd1e4adda7b4198ed0bbf4
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2012-08-29 17:08:50 +0200

    simplier

--- core/test_client.py
+++ core/test_client.py
@@ -9,28 +9,18 @@ if not config:
     exit()
  
 def printValue(value):
-    print pprint.pprint(value)
+    pprint.pprint(value)
 
 def printError(error):
     print ' !! ERROR: ', error
  
 def shutDown(data):
-    print " -> Shutting down reactor..."
+#    print " -> Shutting down reactor..."
     reactor.stop()
 
-command = sys.argv[1]
 proxy = Proxy('127.0.0.1', config['twisted']['port'])
-dl = []
-if len(sys.argv) == 3 :
-    d = proxy.callRemote(command, sys.argv[2])
-elif len(sys.argv) == 7 :
-    d = proxy.callRemote(command, sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
-else :
-    d = proxy.callRemote(command)
+d = proxy.callRemote(sys.argv[1], *sys.argv[2:])
 d.addCallback(printValue).addErrback(printError)
-dl.append(d)
- 
-dl = defer.DeferredList(dl)
-dl.addCallback(shutDown)
+d.addCallback(shutDown)
 reactor.run()
 
