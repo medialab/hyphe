d5ec4b5fc0fae385f3d341a763bad06a40ba319c
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2012-10-18 17:25:28 +0200

    allow command line calls of array functions

--- core/test_client.py
+++ core/test_client.py
@@ -21,8 +21,12 @@ def shutDown(data):
     reactor.stop()
 
 proxy = Proxy('http://127.0.0.1:%d' % config['twisted']['port'])
-print sys.argv[1], sys.argv[2:]
-d = proxy.callRemote(sys.argv[1], *sys.argv[2:])
+if len(sys.argv) > 2 and sys.argv[2] == "array":
+    print sys.argv[1], [a for a in sys.argv[3:]]
+    d = proxy.callRemote(sys.argv[1], [a for a in sys.argv[3:]])
+else:
+    print sys.argv[1], sys.argv[2:]
+    d = proxy.callRemote(sys.argv[1], *sys.argv[2:])
 d.addCallback(printValue).addErrback(printError)
 d.addCallback(shutDown)
 reactor.run()
