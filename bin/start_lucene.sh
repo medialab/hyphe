8eabaa8fc0b476df6e6168dd7d0e08650f087a4f
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2012-10-31 18:25:04 +0100

    opti conf java for lucene

--- bin/start_lucene.sh
+++ bin/start_lucene.sh
@@ -6,5 +6,5 @@ if [ -z $1 ]; then
 else log="log.level=DEBUG"
 fi
 
-java -jar memory_structure/target/MemoryStructureExecutable.jar $log
+java -server -Xms512m -Xmx2048m -Xmn224m -XX:NewSize=224m -XX:MaxNewSize=224m -XX:NewRatio=3 -XX:SurvivorRatio=6 -XX:PermSize=128m -XX:MaxPermSize=128m -XX:+UseParallelGC -XX:ParallelGCThreads=2 -jar memory_structure/target/MemoryStructureExecutable.jar $log
 
