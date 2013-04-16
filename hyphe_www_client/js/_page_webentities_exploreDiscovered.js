bb22058551ef82f6547393726106bcb89fcd22b6
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2013-04-16 11:53:35 +0200

    verbose in exploreDiscovered #www

--- hyphe_www_client/js/_page_webentities_exploreDiscovered.js
+++ hyphe_www_client/js/_page_webentities_exploreDiscovered.js
@@ -1,6 +1,6 @@
 domino.settings({
     shortcutPrefix: "::" // Hack: preventing a bug related to a port in a URL for Ajax
-    ,verbose: false
+    ,verbose: true
 })
 
 ;(function($, domino, dmod, undefined){
