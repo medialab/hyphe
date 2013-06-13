8bb6b8927454172a7adf302eeca1da24393329bb
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2013-06-13 15:48:24 +0200

    lighter notifications #www

--- hyphe_www_client/js/hyphe_commons.js
+++ hyphe_www_client/js/hyphe_commons.js
@@ -8,8 +8,8 @@
 	        alert('Your installation of Hyphe has no configuration.\nCreate a file at "_config/config.js" in the same directory than index.php, with at least this content:\n\nHYPHE_CONFIG = {\n"SERVER_ADDRESS":"http://YOUR_RPC_ENDPOINT_URL"\n}')
 	    }
 	    Messenger.options = {
-			extraClasses: 'messenger-fixed messenger-on-bottom',
-			theme: 'future'
+			extraClasses: 'messenger-fixed messenger-on-top',
+			theme: 'block'
 		}
 	}
 
@@ -29,18 +29,19 @@
 	ns.RPC.error = function(data, xhr, input){
 		// alert('Oops, an error occurred... \n'+data)
 		Messenger().post({
-		    message: 'Oops, an error occurred... '+data
+		    message: '<strong>Oops, an error occurred</strong> when communicating with the server: "'+data+'"'
 		    ,type: 'error'
 		    ,showCloseButton: true
-		    ,actions: {
+		    /*,actions: {
 			    retry: {
 					label: 'Log it in console',
 					action: function() {
 						console.log('RPC error - XHR:', xhr, 'Input:', input)
 					}
 			    }
-			 }
+			}*/
     	})
+    	console.log('RPC error - XHR:', xhr, 'Input:', input)
 	}
 
 	ns.RPC.contentType = 'application/x-www-form-urlencoded'
