7795e5c750728db9d8f9dc7c79d4ba9b5a70a2dd
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2013-07-30 13:30:08 +0200

    more generic sample script to run batch status setting

---
+++ bin/samples/mark_matching_keyword_as_status.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+source /usr/local/bin/virtualenvwrapper.sh
+workon HCI
+
+keyword=$(echo $1 | sed 's/^\(.*\)$/\1/')
+statuses=$(echo $2 | sed 's/^\(.*\)$/\U\1/')
+
+if [ -z "$keyword" ]; then
+  echo "Please provide a keyword (status or tag) you want to search for and set the status to"
+  exit
+fi
+
+if [ -z "$statuses" ]; then
+  echo "Please provide a status you want to set to the searched webentity (IN, OUT, UNDECIDED or DISCOVERED)"
+  exit
+fi
+
+./hyphe_backend/test_client.py inline store.get_webentities |
+  grep result |
+  sed "s/{u'status'/\n{u'status'/g" |
+  grep "'$keyword'" |
+  sed "s/^.*'id': u'//" |
+  sed "s/'}.*$//" |
+  while read id; do
+    ./hyphe_backend/test_client.py store.set_webentity_status "$id" "$statuses"
+  done
+
