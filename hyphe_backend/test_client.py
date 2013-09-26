b8188ee49f025ae5cd957c2de1186a56722879ee
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2013-09-26 17:18:45 +0200

    allow script command_line tester to send commands with array written as string in shell

--- hyphe_backend/test_client.py
+++ hyphe_backend/test_client.py
@@ -6,17 +6,17 @@ Simple script to test the API in command line
 optionnal 1st arg : "inline" to get results as a single line
 2nd arg           : method name
 optionnal 3rd arg : "array" to mark following arguments are to be taken as elements of an array
-following args    : method's arguments
+following args    : method's arguments. To provide an array, write it as a string after "array", for instance : « array "['test','test2']" » or « array "[['test1','test2'],['test3','test4']]] ».
 Examples from HCI root:
 ./hyphe_backend/test_client.py get_status
 ./hyphe_backend/test_client.py declare_page http://medialab.sciences-po.fr
-./hyphe_backend/test_client.py declare_pages array http://medialab.sciences-po.fr http://www.sciences-po.fr
+./hyphe_backend/test_client.py declare_pages array "['http://medialab.sciences-po.fr','http://www.sciences-po.fr']"
 ./hyphe_backend/test_client.py inline store.get_webentities
 """
 
 from twisted.internet import reactor, defer
 from txjsonrpc.web.jsonrpc import Proxy
-import sys
+import sys, re
 from hyphe_backend.lib import config_hci
 
 config = config_hci.load_config()
@@ -48,13 +48,31 @@ def shutdown(data):
     reactor.stop()
 
 proxy = Proxy('http://127.0.0.1:%d' % config['twisted']['port'])
-if len(sys.argv) > startargs and sys.argv[startargs] == "array":
-    print sys.argv[startargs - 1], [a for a in sys.argv[startargs + 1:]]
-    d = proxy.callRemote(sys.argv[startargs - 1], [a for a in sys.argv[startargs + 1:]])
-else:
-    print sys.argv[startargs - 1], sys.argv[startargs:]
-    d = proxy.callRemote(sys.argv[startargs - 1], *sys.argv[startargs:])
+command = sys.argv[startargs - 1]
+args = []
+is_array = False
+for a in sys.argv[startargs:]:
+    if a == "array":
+        is_array = True
+    else:
+        if is_array:
+            if a.startswith('[') and a.endswith(']'):
+                args.append(eval(a))
+            elif not len(a):
+                args.append([])
+            else:
+                args.append([a])
+        else:
+            args.append(a)
+        is_array = False
+
+re_clean_args = re.compile(r"^\[(.*)\]$")
+print "CALL:", command, re_clean_args.sub(r"\1", str(args))
+d = proxy.callRemote(command, *args)
+
 d.addCallback(printValue).addErrback(printError)
 d.addCallback(shutdown)
 reactor.run()
+
+
 
