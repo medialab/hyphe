bcce4fffc68a280391e18f0e5ffe51102c978df1
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2013-12-04 12:14:52 +0100

    add script to get as csv all webentities for a specific status

---
+++ bin/samples/get_webentities_by_status_as_csv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source /usr/local/bin/virtualenvwrapper.sh
+workon HCI
+
+status=$(echo $1 | sed 's/^\(.*\)$/\U\1/')
+if [ -z "$status" ]; then
+  echo "Please provide a status you want to crawl (IN, OUT, UNDECIDED or DISCOVERED)"
+  exit
+fi
+
+echo "status,lru_prefixes,id,name,tags"
+./hyphe_backend/test_client.py inline store.get_webentities "" False False "" False True |
+  grep result |
+  sed "s/{u'status'/\n{u'status'/g" |
+  grep -i "u'status': u'$status'" |
+  sed "s/', u'[^']*': u'/\",\"/g" |
+  sed "s/^{u'[^']*': u'/\"/" |
+  sed "s/'}\(, \|]}\)$/\"/"
+
