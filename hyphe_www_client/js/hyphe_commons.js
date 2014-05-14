99fdd6e361e1a0a3ab5b2d376eb66692e9d8e26c
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2014-05-14 12:13:04 +0200

    #www fixed behavior of definePrefixes

--- hyphe_www_client/js/hyphe_commons.js
+++ hyphe_www_client/js/hyphe_commons.js
@@ -82,7 +82,7 @@
 		settings.httpVariations = settings.httpVariations || false
 		settings.httpsVariations = settings.httpsVariations || false
 		if(settings.smallerVariations === undefined){settings.smallerVariations = true}
-
+		console.log('SETTINGS', settings)
 		candidates.push(lru)
 		if(lru_a.length>2+tld_length && settings.smallerVariations){
 			for(length = lru_a.length-1; length>=2+tld_length; length--){
@@ -101,6 +101,7 @@
 				,wwwVariations: false
 				,httpVariations: settings.httpVariations
 				,httpsVariations: settings.httpsVariations
+				,smallerVariations: settings.smallerVariations
 			}))
 		}
 		if(settings.wwwVariations && lru_json.host[lru_json.host.length - 1] != 'www'){
@@ -110,6 +111,7 @@
 			candidates = candidates.concat(ns.getPrefixCandidates(wwwVariation, {
 				wwwlessVariations: false
 				,wwwVariations: false
+				,smallerVariations: settings.smallerVariations
 				,httpVariations: settings.httpVariations
 				,httpsVariations: settings.httpsVariations
 			}))
@@ -121,6 +123,7 @@
 			candidates = candidates.concat(ns.getPrefixCandidates(httpsVariation, {
 				wwwlessVariations: settings.wwwlessVariations
 				,wwwVariations: settings.wwwVariations
+				,smallerVariations: settings.smallerVariations
 				,httpVariations: false
 				,httpsVariations: false
 			}))
@@ -133,6 +136,7 @@
 			candidates = candidates.concat(ns.getPrefixCandidates(httpVariation, {
 				wwwlessVariations: settings.wwwlessVariations
 				,wwwVariations: settings.wwwVariations
+				,smallerVariations: settings.smallerVariations
 				,httpVariations: false
 				,httpsVariations: false
 			}))
