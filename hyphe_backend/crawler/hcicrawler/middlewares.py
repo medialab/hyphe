12e001f7fdfae60eabc7e32bece5350712b69d79
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-06-20 15:43:31 +0200

    do not use empty proxy

--- hyphe_backend/crawler/hcicrawler/middlewares.py
+++ hyphe_backend/crawler/hcicrawler/middlewares.py
@@ -3,5 +3,5 @@ from hcicrawler.settings import PROXY
 class ProxyMiddleware(object):
     # overwrite process request
     def process_request(self, request, spider):
-        if PROXY != "":
+        if PROXY != "" and not PROXY.startswith(':'):
             request.meta['proxy'] = "http://%s/" % PROXY
