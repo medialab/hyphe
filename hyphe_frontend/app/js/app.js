fb0555561799148d77d87703d7e93338c86e9e99
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2014-08-08 11:38:58 +0200

    starting to template overview

--- hyphe_frontend/app/js/app.js
+++ hyphe_frontend/app/js/app.js
@@ -10,7 +10,7 @@ angular.module('hyphe', [
   'hyphe.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'Login'});
+  $routeProvider.when('/overview', {templateUrl: 'partials/overview.html', controller: 'Overview'});
+  $routeProvider.otherwise({redirectTo: '/login'});
 }]);
--- hyphe_frontend/app/js/controllers.js
+++ hyphe_frontend/app/js/controllers.js
@@ -3,13 +3,10 @@
 /* Controllers */
 
 angular.module('hyphe.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('Login', ['$scope', function($scope) {
 
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  .controller('Overview', ['$scope', function($scope) {
 
-  }])
-  .controller('Project', ['$scope', function($scope) {
-    $scope.activeProjectId = undefined
   }])
 
