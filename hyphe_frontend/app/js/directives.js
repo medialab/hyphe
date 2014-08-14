4180f5b9018ae7f255fc9171bc1f65a1f5ba7dfd
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2014-08-14 15:04:40 +0200

    prefix slider interaction functional

--- hyphe_frontend/app/js/directives.js
+++ hyphe_frontend/app/js/directives.js
@@ -17,21 +17,20 @@ angular.module('hyphe.directives', [])
 	      var opt = scope.$eval(attrs.hyphePrefixSlider) || {}	// allow options to be passed
 	      opt.cursor = opt.cursor || 'move'
 
+        // Keeping an updated
 	      var steps
-        scope.$watch(function(){  // Look at active state (blurred ancestor)
-          var componentRoot = elm.parent().parent().parent().parent()
-          ,container = componentRoot.parent()
-          return componentRoot.hasClass('blurred') || container.hasClass('blurred')
-        }, updateCoordinates)
-        scope.$watch(function(){  // Look at active state (blurred ancestor)
-          // Look at offset
-          return elm.parent().offset().left
-        }, updateCoordinates)
+        scope.$watch(function(){  // Watch active state (blurred ancestor)
+            var componentRoot = elm.parent().parent().parent().parent()
+            ,container = componentRoot.parent()
+            return componentRoot.hasClass('blurred') || container.hasClass('blurred')
+          }, updateCoordinates)
+        scope.$watch(function(){  // Watch coordinates change
+            return elm.parent().offset().left
+          }, updateCoordinates)
+        /*scope.$watch(function(){  // Watch object change
+            return scope.obj
+          }, updateCoordinates)*/
 				$(window).resize(updateCoordinates)
-        /*
-        scope.$watch('obj', updateSteps)
-        scope.$watch('slidersLoaded', updateSteps)
-        */
 	      
 	      var z_idx = elm.css('z-index')
 	      ,drg_w = elm.outerWidth()
@@ -61,9 +60,36 @@ angular.module('hyphe.directives', [])
         }
 
         function updateDrag(e) {
-          $('.draggable').offset({
-              left:Math.min(steps[steps.length-1], Math.max(steps[0], e.pageX + pos_x - drg_w))
+          var x = e.pageX + pos_x - drg_w
+
+          // magnetic steps
+          steps.forEach(function(step, i){
+            if(Math.abs(step - x) < 12){
+              x = step
+            }
           })
+
+          // boundaries
+          x = Math.min(steps[steps.length-1], Math.max(steps[0], x))
+
+          $('.draggable').offset({
+              left:x
+          })
+
+          // update prefix
+          var closestStepId = -1
+          ,cs_dist = Number.MAX_VALUE
+          steps.forEach(function(step, i){
+            var d = Math.abs(step - x)
+            if(d < cs_dist){
+              cs_dist = d
+              closestStepId = i+1
+            }
+          })
+          if(scope.obj.prefixLength != closestStepId){
+            scope.obj.prefixLength = closestStepId
+            scope.$apply()
+          }
         }
 
         function endDrag() {
@@ -72,12 +98,13 @@ angular.module('hyphe.directives', [])
 						.css('z-index', z_idx)
 						.parents()
           		.off("mousemove", updateDrag)
+          updatePosition()
         }
 
         function updateSteps(){
         	steps = elm.parent().find('table>tbody>tr>td').toArray().map(function(td){
         		var $td = $(td)
-        		return $td.offset().left// + $td.width()
+        		return $td.offset().left + $td.outerWidth()
         	})
         }
 
@@ -87,15 +114,21 @@ angular.module('hyphe.directives', [])
           })
         }
 
+        function updatePosition(){
+          elm.offset({
+              left:steps[(scope.obj.prefixLength || 1)-1]
+          })
+        }
+
         function updateCoordinates(){
           var componentRoot = elm.parent().parent().parent().parent()
           ,container = componentRoot.parent()
           if(componentRoot.hasClass('blurred') || container.hasClass('blurred')){
             // we do not check
           } else {
-            console.log('update coordinates for row '+scope.$index)
             updateSteps()
             updateBoundaries()
+            updatePosition()
           }
         }
 	    }
