50863d35a32a8bcc279bd20ce82efc21998e6678
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-08-19 19:05:06 +0200

    better logs handling

--- hyphe_backend/lib/lucene_runner.py
+++ hyphe_backend/lib/lucene_runner.py
@@ -5,13 +5,25 @@ import os, time
 import threading
 import subprocess
 import socket
+from datetime import datetime
 from hyphe_backend.lib import config_hci
 
 config = config_hci.load_config()
 if not config:
     exit()
 
-HYPHE_MS_JAR = "%s/hyphe_backend/memorystructure/MemoryStructureExecutable.jar" % os.getcwd()
+HYPHE_MS_JAR = os.path.join(os.getcwd(), "hyphe_backend", "memorystructure",
+    "MemoryStructureExecutable.jar")
+
+def parse_log(line):
+    line = line.strip("\n")
+    linesplit = line.split(",")
+    if len(linesplit) < 5:
+        raise Exception(
+            "Log from MemoryStructure seems wrongly formatted: %s" % line)
+    lineparsed = linesplit[:4]
+    lineparsed.append(",".join(linesplit[4:]))
+    return lineparsed
 
 class LuceneRunner(threading.Thread):
 
@@ -19,15 +31,35 @@ class LuceneRunner(threading.Thread):
 
     def __init__(self, corpus="test", maxram=512):
         self.corpus = corpus
-        self.ram_options = "-Xms%dm -Xmx%dm" % (max(256, maxram/4), maxram)
-        self.java_options = "-server %s -Xmn224m -XX:NewSize=224m -XX:MaxNewSize=224m -XX:NewRatio=3 -XX:SurvivorRatio=6 -XX:PermSize=128m -XX:MaxPermSize=128m -XX:+UseParallelGC -XX:ParallelGCThreads=2" % self.ram_options
-        self.command = "java %s -jar %s corpus=%s" % (self.java_options, HYPHE_MS_JAR, self.corpus)
+        self.status = "init"
+        self.ram = maxram
+        self.port = 0
         self.proc = None
         threading.Thread.__init__(self)
-        self.port = 0
+
+    def log(self, msg, error=False):
+        logtype = "ERROR" if error else "INFO"
+        logmetas = "%s - %s" % (self.corpus, logtype)
+        print("[%s] %s" % (logmetas, msg))
+        if error:
+            self.stop()
+
+    def stop(self):
+        self.status = "closing"
+        if not self.proc.poll():
+            try:
+                self.proc.terminate()
+                self.monitor()
+            except:
+                pass
+        else:
+            self.log("self.proc.poll != none: %s" % self.proc.poll(), True)
+        self.log("Stopped running")
+        self.status = "closed"
 
     def choose_port(self):
-        address = config['memoryStructure']['thrift.host'].replace('localhost', '')
+        address = config['memoryStructure']['thrift.host']
+        address = address.replace('localhost', '')
         for port in range(*config['memoryStructure']['thrift.portrange']):
             try:
                 s = socket.socket()
@@ -41,26 +73,39 @@ class LuceneRunner(threading.Thread):
     def run(self):
         self.port = self.choose_port()
         if not self.port:
+            self.log("Couldn't find a port to attach MemoryStructure to", True)
             return
-        command = "%s thrift.port=%d" % (self.command, self.port)
-        print command
-        self.proc = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True)
+        java_options = "-Xms%dm -Xmx%dm " % (max(256, self.ram/4), self.ram)
+        java_options += "-Xmn224m -XX:NewSize=224m -XX:MaxNewSize=224m " + \
+            "-XX:NewRatio=3 -XX:SurvivorRatio=6 -XX:PermSize=128m " + \
+            "-XX:MaxPermSize=128m -XX:+UseParallelGC -XX:ParallelGCThreads=2"
+        command = "java -server %s -jar %s corpus=%s thrift.port=%d" % \
+            (java_options, HYPHE_MS_JAR, self.corpus, self.port)
+        self.log("Starting MemoryStructure on port %s with %sMo ram" % \
+            (self.port, self.ram))
+        self.proc = subprocess.Popen(command.split(), stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True)
+        self.status = "started"
         self.monitor()
 
     def monitor(self):
         while self.proc.poll() is None:
-            line = self.proc.stdout.readline().replace("\n", "")
-            if line and "ERROR" in line:
-                print "[%s] %s" % (self.corpus, line)
-        print "DONE"
-
-    def stop(self):
-        if not myclass.proc.poll():
+            line = self.proc.stdout.readline()
+            if not line.strip():
+                continue
             try:
-                self.proc.terminate()
-                self.monitor()
+                lts, ltype, lclass, lthread, msg = parse_log(line)
             except:
-                pass
+                self.log("Internal Error", True)
+            if ltype == "ERROR":
+                self.log(msg, True)
+            elif msg == "shutting down":
+                self.status = "closing"
+                self.log(msg)
+            elif lclass=="LRUIndex" and msg.startswith("starting Thrift server"):
+                self.status = "ready"
+                self.log("MemoryStructure ready")
+        self.status = "stopped"
 
 # TESTING
 if __name__ == '__main__':
