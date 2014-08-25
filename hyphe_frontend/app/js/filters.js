d2d0dc5e99a8debb7a620d4fa417f7e5d68865bf
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2014-08-25 13:09:56 +0200

    Lookup codes

--- hyphe_frontend/app/js/filters.js
+++ hyphe_frontend/app/js/filters.js
@@ -32,5 +32,223 @@ angular.module('hyphe.filters', [])
     }
   }])
 
+  .filter('explicitHttpCode', [function () {
+    return function (code) {
+      code = ''+code
+      switch(code){
+        case('-1'):
+          return 'Hyphe cannot deal with this URL'
+          break
+        case('0'):
+          return 'Domain name cannot not be found'
+          break
+        case('200'):
+          return 'OK'
+          break
+        case('300'):
+          return 'Redirection "Multiple Choices"'
+          break
+        case('301'):
+          return 'Redirection "Moved Permanently"'
+          break
+        case('302'):
+          return 'Redirection "Found"'
+          break
+        case('303'):
+          return 'Redirection "See Other"'
+          break
+        case('304'):
+          return 'Redirection "Not Modified"'
+          break
+        case('305'):
+          return 'Redirection "Use Proxy"'
+          break
+        case('306'):
+          return 'Redirection "Switch Proxy"'
+          break
+        case('307'):
+          return 'Redirection "Temporary Redirect"'
+          break
+        case('308'):
+          return 'Redirection "Permanent Redirect"'
+          break
+        case('400'):
+          return 'Client Error "Bad Request"'
+          break
+        case('401'):
+          return 'Client Error "Unauthorized"'
+          break
+        case('402'):
+          return 'Client Error "Payment Required"'
+          break
+        case('403'):
+          return 'Client Error "Forbidden"'
+          break
+        case('404'):
+          return 'Client Error "Not Found"'
+          break
+        case('405'):
+          return 'Client Error "Method Not Allowed"'
+          break
+        case('406'):
+          return 'Client Error "Not Acceptable"'
+          break
+        case('407'):
+          return 'Client Error "Proxy Authentification Required"'
+          break
+        case('408'):
+          return 'Client Error "Request Timeout"'
+          break
+        case('409'):
+          return 'Client Error "Conflict"'
+          break
+        case('410'):
+          return 'Client Error "Gone"'
+          break
+        case('411'):
+          return 'Client Error "Length Required"'
+          break
+        case('412'):
+          return 'Client Error "Precondition Failed"'
+          break
+        case('413'):
+          return 'Client Error "Request Entity Too Large"'
+          break
+        case('414'):
+          return 'Client Error "Request-URI Too Long"'
+          break
+        case('415'):
+          return 'Client Error "Unsupported Media Type"'
+          break
+        case('416'):
+          return 'Client Error "Requested Range Not Satisfiable"'
+          break
+        case('417'):
+          return 'Client Error "Expectation Failed"'
+          break
+        case('418'):
+          return 'This server appears to be a teapot. True story.'
+          break
+        case('419'):
+          return 'Client Error "Authentication Timeout"'
+          break
+        case('420'):
+          return 'Client Error "Enhance Your Calm / Method Failure"'
+          break
+        case('422'):
+          return 'Client Error "Unprocessable Entity"'
+          break
+        case('423'):
+          return 'Client Error "Locked"'
+          break
+        case('424'):
+          return 'Client Error "Failed Dependency"'
+          break
+        case('426'):
+          return 'Client Error "Upgrade Required"'
+          break
+        case('428'):
+          return 'Client Error "Precondition Required"'
+          break
+        case('429'):
+          return 'Client Error "Too Many Requests"'
+          break
+        case('431'):
+          return 'Client Error "Request Header Fields Too Large"'
+          break
+        case('440'):
+          return 'Client Error "Login Timeout / No Response"'
+          break
+        case('444'):
+          return 'Client Error "No Response"'
+          break
+        case('449'):
+          return 'Client Error "Retry With"'
+          break
+        case('450'):
+          return 'Client Error "Blocked by Windows Parental Controls"'
+          break
+        case('451'):
+          return 'Client Error "Unavailable for Legal Reasons / Redirect"'
+          break
+        case('494'):
+          return 'Client Error "Request Header Too Large"'
+          break
+        case('495'):
+          return 'Client Error "Cert Error"'
+          break
+        case('496'):
+          return 'Client Error "No Cert"'
+          break
+        case('497'):
+          return 'Client Error "HTTP to HTTPS"'
+          break
+        case('498'):
+          return 'Client Error "Token expired/invalid"'
+          break
+        case('499'):
+          return 'Client Error "Client Closed Request / Token Required"'
+          break
+        case('501'):
+          return 'Server Error "Not Implemented"'
+          break
+        case('502'):
+          return 'Server Error "Bad Gateway"'
+          break
+        case('503'):
+          return 'Server Error "Service Unavailable"'
+          break
+        case('504'):
+          return 'Server Error "Gateway Timeout"'
+          break
+        case('505'):
+          return 'Server Error "HTTP Version Not Supported"'
+          break
+        case('506'):
+          return 'Server Error "Variant Also Negotiates"'
+          break
+        case('507'):
+          return 'Server Error "Insufficient Storage"'
+          break
+        case('508'):
+          return 'Server Error "Loop Detected"'
+          break
+        case('509'):
+          return 'Server Error "Bandwidth Limit Exceeded"'
+          break
+        case('510'):
+          return 'Server Error "Not Extended"'
+          break
+        case('511'):
+          return 'Server Error "Network Authentication Required"'
+          break
+        case('520'):
+          return 'Server Error "Origin Error"'
+          break
+        case('521'):
+          return 'Server Error "Web server is down"'
+          break
+        case('522'):
+          return 'Server Error "Connection timed out"'
+          break
+        case('523'):
+          return 'Server Error "Proxy Declined Request"'
+          break
+        case('524'):
+          return 'Server Error "A timeout occurred"'
+          break
+        case('598'):
+          return 'Server Error "Network read timeout error"'
+          break
+        case('599'):
+          return 'Server Error "Network connect timeout error"'
+          break
+
+
+      }
+      return "Lookup Fail"
+    }
+  }])
+
 ;
 
