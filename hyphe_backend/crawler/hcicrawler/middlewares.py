7dfec05fe620a152328902b3caf7d04b366955c8
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2015-04-08 15:45:20 +0200

    clear all

--- hyphe_backend/crawler/hcicrawler/middlewares.py
+++
@@ -1,7 +0,0 @@
-from hcicrawler.settings import PROXY
-
-class ProxyMiddleware(object):
-    # overwrite process request
-    def process_request(self, request, spider):
-        if PROXY != "" and not PROXY.startswith(':'):
-            request.meta['proxy'] = "http://%s/" % PROXY
