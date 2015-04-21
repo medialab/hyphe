0635d798473b3235ab50f18131aaa8284d03ae9f
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2015-04-21 17:36:15 +0200

    sections order

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -13,7 +13,7 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
 		didScroll = false,
-		changeHeaderOn = 750;
+		changeHeaderOn = 800;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
