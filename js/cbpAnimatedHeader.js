8087ee8cec6ae2f75cbe9cee1eb65c360f0605f4
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2015-04-22 18:35:45 +0200

    fix problem with menu

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -13,7 +13,7 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
 		didScroll = false,
-		changeHeaderOn = 800;
+		changeHeaderOn = 150;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
