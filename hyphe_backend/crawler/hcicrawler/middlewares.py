296991c74f2e3fcde35f9963c36d21a4141ffa77
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2016-03-11 13:48:35 +0100

    handles custom dupefilter across various scrapy versions

--- hyphe_backend/crawler/hcicrawler/middlewares.py
+++ hyphe_backend/crawler/hcicrawler/middlewares.py
@@ -1,5 +1,12 @@
+import os
 from hcicrawler.settings import PROXY
-from scrapy.dupefilter import BaseDupeFilter
+try:
+    from scrapy.dupefilters import BaseDupeFilter
+except:
+    from scrapy.dupefilter import BaseDupeFilter
+from scrapy.utils.request import request_fingerprint
+from scrapy.utils.job import job_dir
+from scrapy import log
 
 def use_proxy(request):
     return PROXY != "" and not PROXY.startswith(':') and not request.meta['noproxy']
@@ -10,11 +17,37 @@ class ProxyMiddleware(object):
         if use_proxy(request):
             request.meta['proxy'] = "http://%s/" % PROXY
 
-class CustomDupeFilter(BaseDupeFilter):
-    """Handle duplicates only on same proxy"""
+class RFPDupeFilter(BaseDupeFilter):
+    """Request Fingerprint duplicates filter, handle duplicates only on same proxy"""
 
-    def request_fingerprint(self, request):
+    def __init__(self, path=None):
+        self.file = None
+        self.fingerprints = set()
+        self.logdupes = True
+        if path:
+            self.file = open(os.path.join(path, 'requests.seen'), 'a+')
+            self.fingerprints.update(x.rstrip() for x in self.file)
+
+    @classmethod
+    def from_settings(cls, settings):
+        return cls(job_dir(settings))
+
+    def request_seen(self, request):
         fp = request_fingerprint(request)
         if use_proxy():
              fp += "/P"
-        return fp
+        if fp in self.fingerprints:
+            return True
+        self.fingerprints.add(fp)
+        if self.file:
+            self.file.write(fp + os.linesep)
+
+    def close(self, reason):
+        if self.file:
+            self.file.close()
+
+    def log(self, request, spider):
+        if self.logdupes:
+            fmt = "Filtered duplicate request: %(request)s - no more duplicates will be shown (see DUPEFILTER_CLASS)"
+            log.msg(format=fmt, request=request, level=log.DEBUG, spider=spider)
+            self.logdupes = False
