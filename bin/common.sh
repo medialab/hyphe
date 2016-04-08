2ebb940cadf9e44c7b004e3095ddb69350a3d130
RouxRC <b.ooghe@gmail.com>
2016-04-08 14:52:24 +0200

    consider mint as debian/ubuntu

--- bin/common.sh
+++ bin/common.sh
@@ -11,7 +11,7 @@ exitAndLog() {
 }
 
 isCentOS() {
-  if cat /etc/issue 2> /dev/null | grep -i "debian\|ubuntu" > /dev/null; then
+  if cat /etc/issue 2> /dev/null | grep -i "debian\|ubuntu\|mint" > /dev/null; then
     return 1
   fi
   return 0
@@ -19,7 +19,7 @@ isCentOS() {
 
 isDebian() {
   version=`cat /etc/issue | sed 's/[^0-9.]//g' | awk -F "." '{print $1$2}'`
-  if cat /etc/issue 2> /dev/null | grep -i "debian" > /dev/null; then
+  if cat /etc/issue 2> /dev/null | grep -i "debian\|mint" > /dev/null; then
     return 0
   elif cat /etc/issue 2> /dev/null | grep -i "ubuntu" > /dev/null && [ "$version" -lt 910 ]; then
     return 0
