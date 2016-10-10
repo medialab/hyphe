ad6637d603db20ea564a9fa3e3c8b05446c36154
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2016-10-10 15:05:36 +0200

    build zip textExtracts

---
+++ bin/textExtractor/build_zip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+corpus=
+
+cd outputs
+for dir in html text textArticle textArticleSentences textCanola textDefault; do
+  mkdir -p $dir
+  for we in `ls $corpus/`; do
+    touch $dir/$we.txt
+    ls $corpus/$we/$dir | while read f; do
+      cat $outputs/$we/$dir/$f >> $dir/$we.txt
+    done
+  done
+  tar -czvf $dir $dir.tar.gz
+done
