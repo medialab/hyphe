935d8d8d89c89ca1a954744d64247934e575b563
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2016-10-18 17:42:48 +0200

    add script to zip pages results for exports

---
+++ bin/textExtractor/zip_pages.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+corpus=$1
+method=$2
+
+if [ -z "$corpus" ]; then
+  echo "Please give the corpus id"
+  exit(1)
+fi
+
+if [ -z "$method" ]; then
+  method=textCanola
+fi
+
+cd outputs
+
+mkdir -p $method-pages-$corpus
+
+ls $corpus          |
+ grep -v "done.txt" |
+ while read wid; do
+  if [ -e $corpus/$wid/$method]; then
+    echo $wid
+    mkdir $method-pages-$corpus/$wid
+    ls $corpus/$wid/$method/ |
+     while read pid; do
+      if [ -s $corpus/$wid/$method/$pid ]; then
+        cp $corpus/$wid/$method/$pid $method-pages-$corpus/$wid/$pid.txt
+      fi
+    done
+  fi
+done
+
