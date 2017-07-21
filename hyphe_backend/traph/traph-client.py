c083da49730c4923b56f25e4650038e5a823b260
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2017-07-21 18:39:26 +0200

    traph factory

--- hyphe_backend/traph/traph-client.py
+++ hyphe_backend/traph/traph-client.py
@@ -1,40 +1,146 @@
 import os, json
 from time import time
 from Queue import PriorityQueue
+#from threading import Thread
+from twisted.python import log
 from twisted.internet import reactor
+from twisted.internet.task import LoopingCall
 from twisted.internet.defer import Deferred, inlineCallbacks, returnValue
 from twisted.internet.protocol import Protocol, ProcessProtocol
 from twisted.internet.endpoints import UNIXClientEndpoint, connectProtocol
 
 
-class TraphCorpus():
+class TraphFactory(object):
+
+    # TODO:
+    # handle max started corpus ?
+    # max ram ?
+    # loglevel ?
+    # remove ports from config
+    # change in core all calls to msclients
+
+
+    def __init__(self, max_corpus=0):
+        self.corpora = {}
+        self.max_corpus = max_corpus
+
+    def is_full(self):
+        return self.max_corpus and len(self.corpora) >= self.max_corpus
+
+    def log(self, name, msg, error=False, quiet=False):
+        if quiet and not error:
+            return
+        logtype = "ERROR" if error else "INFO"
+        if reactor.running:
+            log.msg(msg, system="%s - %s" % (logtype, name))
+        else:
+            print("[%s - %s] %s" % (logtype, name, msg))
+        if error and name in self.corpora:
+            self.corpora[name].status = "error"
+            self.corpora[name].error = msg
+
+    def status_corpus(self, name, simplify=False):
+        if name not in self.corpora:
+            return "stopped"
+        if not simplify:
+            return self.corpora[name].status
+        if self.test_corpus(name):
+            return "ready"
+        if self.corpora[name].status == "error":
+            return "error"
+        if self.corpora[name].stopping():
+            return "stopped"
+        return "starting"
+
+    def test_corpus(self, name):
+        return name and self.status_corpus(name) == "ready"
+
+    def starting_corpus(self, name):
+        return name and self.status_corpus(name, True) == "starting"
+
+    def stopped_corpus(self, name):
+        return name not in self.corpora or self.corpora[name].stopping()
+
+    def total_running(self):
+        return len([0 for a in self.corpora if not self.stopped_corpus(a)])
+
+    def start_corpus(self, name, quiet=False, **kwargs):
+        if self.test_corpus(name) or self.status_corpus(name) == "started":
+            self.log(name, "Traph already started", quiet=quiet)
+            return True
+        if name in self.corpora:
+            self.corpora[name].stop()
+            if "keepalive" not in kwargs:
+                kwargs["keepalive"] = self.corpora[name].keepalive
+            del(self.corpora[name])
+        self.corpora[name] = TraphCorpus(self, name, quiet=quiet, **kwargs)
+        if self.is_full():
+            self.log(name, "Too many Traphs already opened", True)
+            return False
+        self.corpora[name].start()
+        return True
+
+    def stop_corpus(self, name, quiet=False):
+        if self.stopped_corpus(name):
+            self.log(name, "Traph already stopped", quiet=quiet)
+            return False
+        if name in self.corpora:
+            self.corpora[name].stop()
+        return True
+
+    def stop(self):
+        for corpus in self.corpora:
+            self.stop_corpus(corpus, True)
+
+    def call(self, corpus, method, *args, **kwargs):
+        if not self.test_corpus(corpus):
+            return {"code": "fail", "message": "Corpus traph not ready"}
+        return self.corpora[corpus].call(method, *args, **kwargs)
+
+class TraphCorpus(object): # Thread ?
 
     sockets_dir = "sockets"
     exec_path = os.path.join("hyphe_backend", "traph", "traph-server.py")
+    #daemon = True
 
-    def __init__(self, corpus, default_WECR=None, WECRs=None):
-        self.corpus = corpus
-        self.socket = os.path.join(self.sockets_dir, corpus)
+    def __init__(self, factory, name, default_WECR=None, WECRs=None, keepalive=1800, quiet=False, **kwargs):
+        #Thread.__init__(self)
+        # TODO check and make socketdirs
+        self.factory = factory
+        self.status = "init"
+        self.name = name
+        self.socket = os.path.join(self.sockets_dir, name)
         self.options = {
           "default_WECR": default_WECR,
           "WECRs": WECRs
         }
+        self.quiet = quiet
+        self.keepalive = keepalive
+        self.lastcall = time()
+        self.call_running = False
+        self.monitor = LoopingCall(self.__check_timeout__)
+        self.error = None
         self.transport = None
         self.protocol = None
         self.client = None
         reactor.addSystemEventTrigger('before', 'shutdown', self.stop)
 
+    #def run(self):
     def start(self):
+        self.error = None
+        self.status = "started"
+        self.log("Starting Traph for at least %ss" % self.keepalive)
         cmd = [
           "python",
           "-u",
           self.exec_path,
           self.socket,
-          self.corpus,
-          json.dumps(self.options)
+          self.name
         ]
-        print "STARTING traph:", " ".join(cmd)
-        self.protocol = TraphProcessProtocol(self.socket)
+        with open(self.socket+"-options.json", "w") as f:
+            json.dump(self.options, f)
+        self.log("Starting Traph: %s" % " ".join(cmd))
+        self.protocol = TraphProcessProtocol(self.socket, self)
         self.transport = reactor.spawnProcess(
           self.protocol,
           cmd[0],
@@ -43,45 +149,78 @@ class TraphCorpus():
         )
         self.client = self.protocol.client
 
+    def call(self, method, *args, **kwargs):
+        return self.client.sendMessage(method, *args, **kwargs)
+
+    def __check_timeout__(self):
+        delay = time() - self.lastcall
+        if self.status == "ready" and self.keepalive < delay and not self.call_running:
+            self.log("Stopping after %ss of inactivity" % int(delay))
+            self.stop()
+
     def stop(self):
+        if self.monitor.running:
+            self.monitor.stop()
+        if self.stopping():
+            return
+        self.status = "error" if self.error else "stopping"
         if self.transport:
             self.protocol.stop()
             self.transport = None
+        self.log("Traph stopped")
+        if not self.error:
+            self.status = "stopped"
 
-    def call(self, method, *args, **kwargs):
-        return self.client.sendMessage(method, *args, **kwargs)
+    def stopping(self):
+        return self.status in ["stopping", "stopped", "error"]
+
+    def log(self, msg, error=False):
+        self.factory.log(self.name, msg, error, quiet=self.quiet)
 
 
 class TraphProcessProtocol(ProcessProtocol):
 
-    def __init__(self, socket):
+    def __init__(self, socket, corpus):
         self.socket = socket
-        self.client = TraphClientProtocol()
+        self.corpus = corpus
+        self.client = TraphClientProtocol(corpus)
 
     def connectionMade(self):
-        print "Process started"
+        self.corpus.status = "starting"
+        print "Traph Process started"
 
+    def connectClient(self):
+        connectProtocol(
+          UNIXClientEndpoint(reactor, self.socket),
+          self.client
+        )
+
+    # TODO handle stack from process
+    # TODO handle traph process cannot start (sock existing for example) -> then hard_restart ?
     def childDataReceived(self, childFD, data):
         data = data.strip()
-        print "PROCESS CHILD %s RECEIVED: %s" % (childFD, data)
+        print "Traph process received \"%s\" on buffer %s" % (data, childFD)
         if childFD == 1 and data == "READY":
-            connectProtocol(
-              UNIXClientEndpoint(reactor, self.socket),
-              self.client
-            )
+            self.connectClient()
 
     def childConnectionLost(self, childFD):
-        print "PROCESS CHILD CONN LOST", childFD
+        print "Traph process lost connection to buffer", childFD
 
+    # TODO handle auto hard restart ?
     def processExited(self, reason):
+        self.corpus.status = "stopping"
         self.transport.loseConnection()
         rc = reason.value.exitCode
         if rc == 0:
-            print "PROCESS CHILD ENDED CLEANLY", rc
+            self.corpus.status = "stopped"
+            print "Traph process exited cleanly"
         else:
-            print "PROCESS CHILD ENDED BADLY:", reason
+            self.corpus.status = "error"
+            self.corpus.error = reason
+            print "Traph process crashed:", reason
 
     def stop(self):
+        self.corpus.status = "stopping"
         if self.transport.pid:
             self.transport.signalProcess("TERM")
 
@@ -93,31 +232,34 @@ TraphMethodPriority = lambda method: TraphMethodsPriorities.get(method, 0)
 
 class TraphClientProtocol(Protocol):
 
-    def __init__(self):
-        self.ready = False
+    def __init__(self, corpus):
+        self.corpus = corpus
         self.deferred = None
         self.queue = PriorityQueue()
 
     def connectionMade(self):
-        print "Connection established with traph"
-        self.ready = True
+        self.corpus.log("Traph ready")
+        self.corpus.status = "ready"
+        self.corpus.monitor.start(max(1, int(self.corpus.keepalive/6)))
 
     def sendMessage(self, method, *args, **kwargs):
         deferred = Deferred()
         priority = TraphMethodPriority(method)
+        self.corpus.lastcall = time()
         self.queue.put(
           (priority, time(), deferred, method, args, kwargs),
           False
         )
-        if self.ready and not self.deferred:
+        if self.corpus.status == "ready" and not self.deferred:
             self._sendMessageNow()
         return deferred
 
     def _sendMessageNow(self):
         if self.queue.empty():
             return
+        self.corpus.call_running = True
         _, _, self.deferred, method, args, kwargs = self.queue.get(False)
-        print "CLIENT QUERY :", method, args, kwargs
+        self.corpus.log("Traph client query: %s %s %s" % (method, args, kwargs))
         self.transport.writeSequence(json.dumps({
           "method": method,
           "args": args,
@@ -126,30 +268,32 @@ class TraphClientProtocol(Protocol):
 
     def dataReceived(self, data):
         data = data.strip()
-        print "SERVER ANSWER:", data
+        self.corpus.log("Traph server answer: %s" % data)
+        self.corpus.lastcall = time()
         try:
             msg = json.loads(data)
             self.deferred.callback(data)
         except ValueError:
-            print >> sys.stderr, "ERROR received non json data", data
+            self.corpus.log("Received non json data %s" % data, True)
             self.deferred.errback(Exception(data))
+        self.corpus.call_running = False
         self.deferred = None
         self._sendMessageNow()
 
 
-@inlineCallbacks
-def testClient(corpus, msg="TEST", *args, **kwargs):
-    res = yield corpus.call(msg, *args, **kwargs)
-    print "YEEEHA", res
-
 if __name__ == "__main__":
     import sys
 
-    corpus = TraphCorpus(sys.argv[1])
-    corpus.start()
+    corpus = "test"
+    if len(sys.argv):
+        corpus = sys.argv[1]
+    factory = TraphFactory()
+    factory.start_corpus(corpus, keepalive=10)
 
-    reactor.callLater(1, testClient, corpus)
-    reactor.callLater(1, testClient, corpus, "clear")
-    reactor.callLater(2, testClient, corpus, "add_page", "s:http|h:fr|h:scpo|p:bib|")
-    reactor.callLater(3, testClient, corpus, "TEST 4")
+    reactor.callLater(2, factory.corpora[corpus].call, "TEST")
+    reactor.callLater(2, factory.corpora[corpus].call, "clear")
+    reactor.callLater(3, factory.corpora[corpus].call, "TEST")
+    reactor.callLater(3, factory.corpora[corpus].call, "add_page", "s:http|h:fr|h:scpo|p:bib|")
+    reactor.callLater(3, factory.corpora[corpus].call, "TEST")
+    reactor.callLater(4, factory.corpora[corpus].call, "TEST 4")
     reactor.run()
--- hyphe_backend/traph/traph-server.py
+++ hyphe_backend/traph/traph-server.py
@@ -14,18 +14,17 @@ class TraphProtocol(Protocol):
         pass
 
     def returnResult(self, res, query):
-        print res
         if isinstance(res, TraphWriteReport):
             res = res.__dict__()
         self.transport.writeSequence(json.dumps({
-          "status": "OK",
+          "code": "success",
           "result": res,
           "query": query
         }))
 
     def returnError(self, msg, err, query):
         self.transport.writeSequence(json.dumps({
-          "status": "fail",
+          "code": "fail",
           "message": msg,
           "error": str(err),
           "query": query
@@ -34,23 +33,23 @@ class TraphProtocol(Protocol):
     def dataReceived(self, query):
         query = query.strip()
         try:
-            data = json.loads(query)
+            query = json.loads(query)
         except ValueError as e:
             return self.returnError("Query is not a valid JSON object", e, query)
         try:
-            method = data["method"]
-            args = data["args"]
-            kwargs = data["kwargs"]
+            method = query["method"]
+            args = query["args"]
+            kwargs = query["kwargs"]
         except KeyError as e:
-            return self.returnError("Argument missing from JSON query", e, data)
+            return self.returnError("Argument missing from JSON query", e, query)
         try:
             fct = getattr(Traph, method)
         except AttributeError as e:
-            return self.returnError("Called non existing Traph method %s" % method, e, data)
+            return self.returnError("Called non existing Traph method %s" % method, e, query)
         try:
             res = fct(self.traph, *args, **kwargs)
         except AttributeError as e:
-            return self.returnError("Badly called Traph method %s" % method, e, data)
+            return self.returnError("Badly called Traph method %s" % method, e, query)
         return self.returnResult(res, query)
 
 
@@ -59,7 +58,7 @@ class TraphProtocol(Protocol):
         pass
 
 
-class TraphFactory(Factory):
+class TraphServerFactory(Factory):
 
     traph_dir = "traph-data"
     default_WECR = '(s:[a-zA-Z]+\\|(t:[0-9]+\\|)?(h:[^\\|]+\\|(h:[^\\|]+\\|)|h:(localhost|(\\d{1,3}\\.){3}\\d{1,3}|\\[[\\da-f]*:[\\da-f:]*\\])\\|))'
@@ -72,9 +71,7 @@ class TraphFactory(Factory):
         self.traph = Traph(
           folder=os.path.join(self.traph_dir, corpus),
           default_webentity_creation_rule=default_WECR or self.default_WECR,
-          webentity_creation_rules=WECRs or self.WECRs,
-          # TODO remove for prod when clear functional
-          overwrite=True
+          webentity_creation_rules=WECRs or self.WECRs
         )
         # stdin message received by childprocess to know when traph is ready
         print "READY"
@@ -89,11 +86,11 @@ if __name__ == "__main__":
     sock = sys.argv[1]
     corpus = sys.argv[2]
     try:
-        options = json.loads(sys.argv[3])
+        with open(sock+"-options.json") as f:
+            options = json.load(f)
     except:
         options = {}
-        pass
-    traph = TraphFactory(corpus, **options)
+    traph = TraphServerFactory(corpus, **options)
     endpoint = UNIXServerEndpoint(reactor, sock)
     endpoint.listen(traph)
     reactor.run()
