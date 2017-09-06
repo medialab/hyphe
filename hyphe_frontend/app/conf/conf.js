361f120aa9fd988a5f7c6f15d0233d05d1162871
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2017-09-06 16:08:25 +0200

    load old js

---
+++ hyphe_frontend/app/conf/conf.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*
+ *	NB: The Hyphe configuration is written as an Angular service
+ */
+
+angular.module('hyphe.conf', [])
+
+  // .constant('serverURL', 'http://hyphe.medialab.sciences-po.fr/dimeweb-api/')
+  .constant('serverURL', 'http://hyphe.medialab.sciences-po.fr/dev-mathieu-api/')
+  .constant('googleAnalyticsId', '')
+  .constant('disclaimer', '')
+
+;
---
+++ hyphe_frontend/app/conf/conf_default.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*
+ *	NB: The Hyphe configuration is written as an Angular service
+ */
+
+angular.module('hyphe.conf', [])
+
+  .constant('serverURL', '/##WEBPATH##-api/')
+  .constant('googleAnalyticsId', '')
+  .constant('disclaimer', '')
+
+
+;
