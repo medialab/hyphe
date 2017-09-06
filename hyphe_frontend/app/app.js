cde68f89bed553dcbe5f1a7c5106cceb00c3cc19
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2017-09-06 16:01:17 +0200

    load assets

--- hyphe_frontend/app/app.js
+++ hyphe_frontend/app/app.js
@@ -3,4 +3,20 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute'
+  ,'ngMessages'
+  ,'ngSanitize'
+  ,'ngMaterial'
+  ,'angulartics'
+  ,'angulartics.google.tagmanager'
+  ,'xeditable'
+  ,'ngTagsInput'
+  // ,'hyphe.analytics'
+  // ,'hyphe.conf'
+  // ,'hyphe.filters'
+  // ,'hyphe.services'
+  // ,'hyphe.service_utils'
+  // ,'hyphe.service_hyphe_api'
+  // ,'hyphe.service_glossary'
+  // ,'hyphe.directives'
+  // ,'hyphe.controllers'
 ])
