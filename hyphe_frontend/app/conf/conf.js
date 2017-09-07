6edae01b07190f8bd7b041cc12724cb56ef38355
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2017-09-07 15:56:01 +0200

    disclaimer and fix sidebar

--- hyphe_frontend/app/conf/conf.js
+++ hyphe_frontend/app/conf/conf.js
@@ -9,6 +9,6 @@ angular.module('hyphe.conf', [])
   // .constant('serverURL', 'http://hyphe.medialab.sciences-po.fr/dimeweb-api/')
   .constant('serverURL', 'http://hyphe.medialab.sciences-po.fr/dev-mathieu-api/')
   .constant('googleAnalyticsId', '')
-  .constant('disclaimer', '')
+  .constant('disclaimer', 'This is a restricted version of Hyphe for demonstration and testing only. Crawls are limited to a 1-click depth. Contents are deleted every week.')
 
 ;
