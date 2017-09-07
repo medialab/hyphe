c3f856005687da1c5f838abde692d2398552a541
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2017-09-07 11:04:20 +0200

    auto focus on fields

--- hyphe_frontend/app/js/directives.js
+++ hyphe_frontend/app/js/directives.js
@@ -387,21 +387,20 @@ angular.module('hyphe.directives', [])
   }])
 
 
-  .directive('focusAfterCollapse', [function(){
-    return{
-      restrict: 'A',
-      link: function(scope, el){
-        scope.$watch(function() {
-            return el.attr('class')
-          }, function(){
-            if (!el.hasClass('collapsing') && el.hasClass('in')) {
-              el.find("input").first().focus()
-            }
+  .directive('focusMe', function($timeout) {
+    return {
+      scope: { trigger: '@focusMe' },
+      link: function(scope, element) {
+        scope.$watch('trigger', function(value) {
+          if(value === "true") { 
+            $timeout(function() {
+              element[0].focus()
+            })
           }
-        )
+        })
       }
     }
-  }])
+  })
 
   .directive('hypheStatusBox', [function(){
     return {
