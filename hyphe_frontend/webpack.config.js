52540456c89f1602747c57c50cd9d0bb10822cce
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2017-09-28 11:50:56 +0200

    packaging fixes

--- hyphe_frontend/webpack.config.js
+++ hyphe_frontend/webpack.config.js
@@ -2,13 +2,5 @@ module.exports = {
   entry: './graphology-builder.js',
   output: {
     filename: './app/graphology-build.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
   }
 };
