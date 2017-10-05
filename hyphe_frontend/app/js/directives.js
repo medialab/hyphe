fc3efe84f263fdd69f043292b1ee646c3ea468b6
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2017-10-05 13:41:23 +0200

    additional activity chart

--- hyphe_frontend/app/js/directives.js
+++ hyphe_frontend/app/js/directives.js
@@ -945,7 +945,7 @@ angular.module('hyphe.directives', [])
               
               window.el = el[0]
               // Setup: dimensions
-              var margin = {top: 0, right: 64, bottom: 32, left: 0};
+              var margin = {top: 8, right: 64, bottom: 32, left: 0};
               var width = el[0].offsetWidth - margin.left - margin.right;
               var height = el[0].offsetHeight - margin.top - margin.bottom;
 
@@ -1023,6 +1023,123 @@ angular.module('hyphe.directives', [])
                   .attr("transform", "translate(" + width + ", 0)")
                   .call(d3.axisRight(y));
 
+            })
+          }
+        }
+
+        function regionValid(d) {
+          return $scope.statuses[d] && $scope.statuses[d].available
+        }
+      }
+    }
+  })
+
+  .directive('activityChart2', function(
+    $timeout
+  ){
+    return {
+      restrict: 'E',
+      scope: {
+        data: '='
+      },
+      link: function($scope, el, attrs) {
+
+        el.html('<div>LOADING</div>')
+
+        $scope.$watch('data', redraw)
+
+        window.addEventListener('resize', redraw)
+        $scope.$on('$destroy', function(){
+          window.removeEventListener('resize', redraw)
+        })
+
+        // Data: timestamp in undecided out discovered in_uncrawled in_untagged total
+        function redraw() {
+          if ($scope.data !== undefined){
+            $timeout(function(){
+              el.html('');
+              
+              window.el = el[0]
+              // Setup: dimensions
+              var margin = {top: 16, right: 64, bottom: 32, left: 0};
+              var width = el[0].offsetWidth - margin.left - margin.right;
+              var height = el[0].offsetHeight - margin.top - margin.bottom;
+
+              // While loading redraw may trigger before element being properly sized
+              if (width <= 0 || height <= 0) {
+                $timeout(redraw, 250)
+                return
+              }
+
+              // Setup: scales
+              var x = d3.scaleTime()
+                .domain(d3.extent($scope.data, function(d) { return new Date(d.timestamp) }))
+                .range([0, width])
+
+              var y = d3.scaleLinear()
+                .domain(d3.extent($scope.data, function(d){return d.in}))
+                .range([height, 0])
+
+              var colorize = function(type){
+                if (type=='in') return '#333'
+                if (type=='in_uncrawled') return '#dcba75'
+                if (type=='in_untagged') return '#75dcbd'
+                return '#ff699b' // Error
+              }
+
+              var line = d3.line()
+                .curve(d3.curveBasis)
+                .x(function(d) { return x(new Date(d.timestamp)) })
+                .y(function(d) { return y(d.value) })
+
+              var curves = ['in', 'in_uncrawled', 'in_untagged'].map(function(k){
+                return {
+                  type: k,
+                  values: $scope.data.map(function(d){
+                    return {timestamp: d.timestamp, value: d[k]}
+                  })
+                }
+              })
+
+              // Setup: SVG container
+              var svg = d3.select(el[0]).append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+              
+              var g = svg.append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+              g.append("g")
+                  .attr("class", "axis axis--x")
+                  .attr("transform", "translate(0," + height + ")")
+                  .call(d3.axisBottom(x));
+
+              g.append("g")
+                  .attr("class", "axis axis--y")
+                  .attr("transform", "translate(" + width + ", 0)")
+                  .call(d3.axisRight(y))
+
+              var curve = g.selectAll(".curve")
+                .data(curves)
+                .enter().append("g")
+                  .attr("class", "curve")
+
+              curve.append("path")
+                  .attr("class", "line")
+                  .attr("d", function(d) { return line(d.values) })
+                  .style("stroke", function(d) { return colorize(d.type) })
+                  .style("stroke-width", "2px")
+                  .style("fill", "none")
+
+              curve.append("text")
+                  .datum(function(d) { return {type: d.type, value: d.values[d.values.length - 1]} })
+                  .attr("transform", function(d) { return "translate(" + (x(new Date(d.value.timestamp)) - ((d.type=='in')?(10):(20)) ) + "," + (y(d.value.value) - 10) + ")"; })
+                  .attr("x", 3)
+                  .attr("dy", "0.35em")
+                  .style("font", "10px sans-serif")
+                  .style("text-anchor", "end")
+                  .style("fill", function(d) { return colorize(d.type) })
+                  .text(function(d) { return d.type });
             })
           }
         }
