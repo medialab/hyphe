6968e7f36c6c3a8cd864611185e97d5e5e138e91
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2017-10-10 11:06:32 +0200

    CLEANUP! (remove old deprecated files)

--- hyphe_www_client/_config_default/config.js
+++
@@ -1,4 +0,0 @@
-HYPHE_CONFIG = {
-	"SERVER_ADDRESS":"http://localhost/##WEBPATH##-api",
-	"JAVASCRIPT_LOG_VERBOSE":false
-}
--- hyphe_www_client/includes/codetop.php
+++
@@ -1,8 +0,0 @@
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-
-        <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
-        <link rel="stylesheet" href="css/bootstrap-editable.css">
-
-        <link rel="stylesheet" href="css/messenger.css">
-        <link rel="stylesheet" href="css/messenger-spinner.css">
-        <link rel="stylesheet" href="css/messenger-theme-future.css">
