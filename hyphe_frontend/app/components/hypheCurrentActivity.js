7df761aaedd4305a42891de66fe6adf239b93ec8
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2017-10-15 10:32:26 +0200

    tweaks

--- hyphe_frontend/app/components/hypheCurrentActivity.js
+++ hyphe_frontend/app/components/hypheCurrentActivity.js
@@ -12,20 +12,47 @@ angular.module('hyphe.hypheCurrentActivityComponent', [])
       },
       templateUrl: 'components/hypheCurrentActivity.html',
       link: function($scope, el, attrs) {
+        var statusListSize = 10
+
+        $scope.statusList = []
+        $scope.isCrawling = false
+
+        $scope.$watch('status', function(newStatus, oldStatus){
+          var crawledPagesBefore = 0
+          var crawledPagesAfter = 0
+          if (oldStatus && oldStatus.crawler && oldStatus.crawler.pages_crawled) {
+            crawledPagesBefore = oldStatus.crawler.pages_crawled
+          }
+          if (newStatus && newStatus.crawler && newStatus.crawler.pages_crawled) {
+            crawledPagesAfter = newStatus.crawler.pages_crawled
+          }
+          $scope.isCrawling = crawledPagesAfter > crawledPagesBefore
+        })
         
-        $scope.$watch('status', redraw)
+      }
+    }
+  })
+
+.directive('hcaCrawledPagesChart', function(
+    $timeout
+  ){
+    return {
+      restrict: 'A',
+      scope: {
+        statusList: '='
+      },
+      link: function($scope, el, attrs) {
+        
+        $scope.$watch('status', update)
         window.addEventListener('resize', redraw)
         $scope.$on('$destroy', function(){
           window.removeEventListener('resize', redraw)
         })
 
         function redraw() {
-          redrawCrawledPages()
-          // TODO: other charts
         }
 
-        function redrawCrawledPages() {
-          
+        function update() {
         }
       }
     }
