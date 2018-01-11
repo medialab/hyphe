544fd57fad5c4f4b627af941dc0e8e1b44c26957
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2018-01-11 18:22:44 +0100

    remove deprecated scripts

--- bin/update_tlds_list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "// Obtained there on the "$(date +%Y-%m-%d)": https://publicsuffix.org/list/
-" > hyphe_frontend/app/res/tld_list.tmp 
-
-curl -sL "https://publicsuffix.org/list/effective_tld_names.dat" >> hyphe_frontend/app/res/tld_list.tmp
-
-mv hyphe_frontend/app/res/tld_list.t{mp,xt}
