28234798186cf883e38782b11835b4f2ba14af8e
Jules <julesfarjas@gmail.com>
2018-08-31 12:27:50 +0200

    Converted indentation to spaces & corrected the line by line write into user_agents.txt

--- hyphe_backend/lib/user_agents.py
+++ hyphe_backend/lib/user_agents.py
@@ -1,51 +1,65 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 from fake_useragent import UserAgent, FakeUserAgentError
 import collections
-import os
+
+user_agent_client = None
+user_agent_list = []
+
+def init_user_agent():
+    directory = os.path.dirname(__file__)
+    path_to_file = os.path.join(directory,"user_agents.txt")
+    try:
+        user_agent_client = UserAgent(cache=False)
+    except FakeUserAgentError:
+        user_agent_list = open('file.txt').read().splitlines()
+        print "Error when trying to instanciate a user-agent with FakeUserAgent. Swintching to local list"
+
+init_user_agent()
 
 def generate_random_useragent():
-	directory = os.path.dirname(__file__)
-	path_to_file = os.path.join(directory,"user_agents.txt")
+    directory = os.path.dirname(__file__)
+    path_to_file = os.path.join(directory,"user_agents.txt")
 
-	try:
-		ua = UserAgent(cache=False)
-	except FakeUserAgentError:
-		print "Error when trying to instanciate a user-agent with FakeUserAgent. Swintching to local list"
-	return ua.random
+    #if user_agent_client is None:
+        #TODO
+    return user_agent_client.random
+
+def update_useragent_list():
+    directory = os.path.dirname(__file__)
+    path_to_file = os.path.join(directory,"user_agents.txt")
+    try:
+        ua = UserAgent(cache=False)
+        ua.update()
+    except FakeUserAgentError:
+        print "Error when trying to update the user-agents list with FakeUserAgent"
+        sys.exit(1)
+
+    # Generating a new list of 100 user agents
+
+    user_agents_list = []
+    for x in range(100):
+        useragent = ua.random
+        while useragent in user_agents_list:
+            useragent=ua.random
+        user_agents_list.append(useragent)
+    print "List of user agents successfully generated"
+
+    # Storing the list into user_agents.txt
+
+    user_agents_file = open(path_to_file, "w")
+    nb_lines = 0
+    for user_agent in user_agents_list:
+        print >> user_agents_file, user_agent
+        nb_lines += 1
+    if nb_lines == 100:
+        print "List of user agents successfully stored in user_agents.txt"
+    else:
+        print "Error storing the list in user_agents.txt"
 
 if __name__ == '__main__':
-	directory = os.path.dirname(__file__)
-	path_to_file = os.path.join(directory,"user_agents.txt")
-	try:
-		ua = UserAgent(cache=False)
-		ua.update()
-	except FakeUserAgentError:
-		print "Error when trying to update the user-agents list with FakeUserAgent"
-		sys.exit(1)
-
-	# Generating a new list of 100 user agents
-
-	user_agents_list = []
-	for x in range(100):
-		ua = UserAgent()
-		useragent = ua.random
-		while useragent in user_agents_list:
-			useragent=ua.random
-		user_agents_list.append(useragent)
-	print "List of user agents successfully generated"
-
-	# Storing the list into user_agents.txt
-
-	user_agents_file = open(path_to_file, "w")
-	nb_lines = 0
-	for user_agent in user_agents_list:
-		user_agents_file.write(user_agent)
-		nb_lines += 1
-	if nb_lines == 100:
-		print "List of user agents successfully stored in user_agents.txt"
-	else:
-		print "Error storing the list in user_agents.txt"
+    update_useragent_list()
 
