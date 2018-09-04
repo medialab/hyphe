05146c5e2d1cdac33ce95e349eacc3154e1475ef
farjasju <julesfarjas@gmail.com>
2018-09-04 10:56:56 +0200

    Name & sort fixes

--- hyphe_backend/lib/user_agents.py
+++ hyphe_backend/lib/user_agents.py
@@ -7,7 +7,7 @@ import random
 from fake_useragent import UserAgent, FakeUserAgentError
 
 USER_AGENTS_CLIENT = None
-user_agents_list = []
+USER_AGENT_LIST = []
 
 # Initializing the UserAgent object if possible, otherwise the user_agets_list from the user_agents.txt file
 
@@ -18,14 +18,14 @@ try:
     USER_AGENTS_CLIENT = UserAgent(cache=False) #Instantiation of the UserAgent object
 except FakeUserAgentError:
     print "Error when trying to instantiate a user-agent with FakeUserAgent. Switching to local list"
-    with open(path_to_file) as f: #Transcription of the local user_agents.txt into the user_agents_list
-        user_agents_list = f.read().splitlines()
+    with open(path_to_file) as f: #Transcription of the local user_agents.txt into the USER_AGENT_LIST
+        USER_AGENT_LIST = f.read().splitlines()
 
 def get_random_user_agent():
     """Returns a random user agent"""
 
     if USER_AGENTS_CLIENT is None:
-        random_user_agent = random.choice(user_agents_list)
+        random_user_agent = random.choice(USER_AGENT_LIST)
     else:
         random_user_agent = USER_AGENTS_CLIENT.random
     return random_user_agent
@@ -45,7 +45,7 @@ def update_user_agents_list():
     new_user_agents_set = set() # Using a set avoids duplicates
     while len(new_user_agents_set) < 100:
         new_user_agents_set.add(USER_AGENTS_CLIENT.random)
-    sorted(new_user_agents_set)
+    new_user_agents_list = sorted(new_user_agents_set)
 
     print "List of user agents successfully generated"
 
@@ -53,7 +53,7 @@ def update_user_agents_list():
 
     try:
         with open(path_to_file, "w") as user_agents_file:
-            for user_agent in new_user_agents_set:
+            for user_agent in new_user_agents_list:
                 print >> user_agents_file, user_agent
     except:
         print "Error writing in user_agents.txt"
