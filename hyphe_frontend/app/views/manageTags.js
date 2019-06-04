419da1b9e9774a3bf29534de1eb76f3b0b2b22a5
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2019-06-04 14:15:25 +0200

    sort webentities displayed in Tags by name

--- hyphe_frontend/app/views/manageTags.js
+++ hyphe_frontend/app/views/manageTags.js
@@ -242,7 +242,11 @@ angular.module('hyphe.manageTagsController', [])
           $scope.searchQuery, false, 'name'
         ),
         $scope.filters, $scope.tagCategories
-      )
+      ).sort(function (a,b) {
+        if (a.name < b.name) return -1;
+        else if (a.name > b.name) return 1;
+        return 0;
+      })
     }
 
     function updateTags() {
