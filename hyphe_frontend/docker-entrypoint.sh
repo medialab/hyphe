3fe5edbbc57a2f9027c4ec7926d06c173a0a2530
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2019-06-06 11:23:42 +0200

    fix double slash in docker front conf

--- hyphe_frontend/docker-entrypoint.sh
+++ hyphe_frontend/docker-entrypoint.sh
@@ -7,7 +7,7 @@ CONFIGFILE=/frontend/app/conf/conf.js
 /bin/cp /frontend/app/conf/conf_default.js $CONFIGFILE
 
 # This sets the API url & handle the case of microsoft browsers not handling CORS correctly
-sed --in-place "s|'serverURL'\s*,.*|'serverURL', window.location.pathname === '/' ? '/api/' : window.location.pathname + '/api/')|" $CONFIGFILE
+sed --in-place "s|'serverURL'\s*,.*|'serverURL', window.location.pathname === '/' ? '/api/' : window.location.pathname.replace(/\/$/, '') + '/api/')|" $CONFIGFILE
 
 [[ ! -z "${HYPHE_GOOGLE_ANALYTICS_ID}" ]] &&
   sed --in-place "s|'googleAnalyticsId'\s*,.*|'googleAnalyticsId', '${HYPHE_GOOGLE_ANALYTICS_ID}')|" $CONFIGFILE
