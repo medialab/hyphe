medialab/hyphe

commit 6fa1ebc0bdae16f789665774d8100d8b606cbc5f

Upgrading graphology & sigma

hyphe_frontend/app/views/webentity_pagesNetwork.js
@@ -187,8 +187,13 @@ angular.module('hyphe.webentityPagesNetworkController', [])
       })
 
       var g = new Graph({type: 'directed', allowSelfLoops: false})
-      g.addNodesFrom(nIndex)
-      g.importEdges(links)
+
+      for (var k in nIndex)
+        g.addNode(k, nIndex[k])
+
+      validLinks.forEach(function(l) {
+        g.importEdge(l)
+      })
 
       // Default appearance
       g.nodes().forEach(function(nid){

hyphe_frontend/app/components/sigmaNetwork.js
@@ -142,10 +142,10 @@ angular.module('hyphe.sigmaNetworkComponent', [])
                 $scope.onNodeClick(e.node)
               })
             })
-            renderer.on('overNode', function(e){
+            renderer.on('enterNode', function(e){
               el[0].classList.add('pointable')
             })
-            renderer.on('outNode', function(e){
+            renderer.on('leaveNode', function(e){
               el[0].classList.remove('pointable')
             })
 

hyphe_frontend/app/components/webentitiesNetworkWidget.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('hyphe.webentitiesNetworkWidgetComponent', [])
- 
+
 .directive('webentitiesNetworkWidget', function(
     $mdSidenav,
     api,
@@ -155,7 +155,7 @@ angular.module('hyphe.webentitiesNetworkWidgetComponent', [])
         $scope.applySettings()
 
         /// Functions
-        
+
         function updateNetworkAppearance() {
           updateNodeColors()
           updateNodeSizes()
@@ -252,7 +252,7 @@ angular.module('hyphe.webentitiesNetworkWidgetComponent', [])
         function updateNodeSizes() {
           var g = $scope.network
           if (g === undefined) { return }
-            
+
           var minSize = 1
           var values = []
           g.nodes().forEach(function(nid){
@@ -441,8 +441,13 @@ angular.module('hyphe.webentitiesNetworkWidgetComponent', [])
             })
 
           var g = new Graph({type: 'directed', allowSelfLoops: false})
-          g.addNodesFrom(weIndex)
-          g.importEdges(validLinks)
+
+          for (var k in weIndex)
+            g.addNode(k, weIndex[k])
+
+          validLinks.forEach(function(l) {
+            g.importEdge(l)
+          })
 
           // Filtering: mark nodes for deletion
           var allThreshold = 0
@@ -453,14 +458,13 @@ angular.module('hyphe.webentitiesNetworkWidgetComponent', [])
           if ($scope.settings.limitDiscovered) {
             discThreshold = +$scope.settings.limitDiscovered.replace('+', '')
           }
-          var nodesToDelete = []
+
           g.nodes().forEach(function(nid){
             var degree = g.degree(nid)
             if (degree < allThreshold || (g.getNodeAttribute(nid, "status") == 'DISCOVERED' && degree < discThreshold)) {
-              nodesToDelete.push(nid)
+              g.dropNode(nid)
             }
           })
-          g.dropNodes(nodesToDelete)
 
           var nodesArea = g.order * 10
           g.nodes().forEach(function(nid){
@@ -544,4 +548,4 @@ angular.module('hyphe.webentitiesNetworkWidgetComponent', [])
     }
   })
 
-  
+

hyphe_frontend/app/views/manageTags.js
@@ -120,7 +120,7 @@ angular.module('hyphe.manageTagsController', [])
         })
       })
       buildTagData()
-      
+
       return api.removeTag_webentities({
           webentityId_list: webentities.map(function(we){return we.id})
           ,category: tagCat
@@ -143,12 +143,12 @@ angular.module('hyphe.manageTagsController', [])
         return false
       }
       $scope.tagCategories[category] = []
-      
+
       // Wait a frame to render the new category before resetting the form field and focus on input
       $timeout(function(){
         $scope.newCategory = ''
       }, 0)
-      
+
       return true
     }
 
@@ -248,7 +248,7 @@ angular.module('hyphe.manageTagsController', [])
     function updateTags() {
       if($scope.loading) { return }
       $scope.filters = []
-      
+
       if ($scope.generalOption !== undefined) {
         // There is a general option (special option)
         // As a consequence everything else is unselected
@@ -421,7 +421,7 @@ angular.module('hyphe.manageTagsController', [])
             ,page: 0
           }
           ,function(result){
-            
+
             $scope.data.in.total = result.total_results
             $scope.data.in.token = result.token
 
@@ -495,8 +495,13 @@ angular.module('hyphe.manageTagsController', [])
         })
 
       var g = new Graph({type: 'directed', allowSelfLoops: false})
-      g.addNodesFrom(weIndex)
-      g.importEdges(validLinks)
+
+      for (var k in weIndex)
+        g.addNode(k, weIndex[k])
+
+      validLinks.forEach(function(l) {
+        g.importEdge(l)
+      })
 
       g.nodes().forEach(function(nid){
         var n = g.getNodeAttributes(nid)

hyphe_frontend/app/views/tool_networkTagStats.js
@@ -119,7 +119,7 @@ angular.module('hyphe.toolNetworkTagStatsController', [])
         })
         attData.values.forEach(function(v1){
           attData.values.forEach(function(v2){
-            attData.valueFlow[v1][v2].nd = ( attData.valueFlow[v1][v2].count - attData.valueFlow[v1][v2].expected ) / (4 * g.size) 
+            attData.valueFlow[v1][v2].nd = ( attData.valueFlow[v1][v2].count - attData.valueFlow[v1][v2].expected ) / (4 * g.size)
           })
         })
 
@@ -356,8 +356,13 @@ angular.module('hyphe.toolNetworkTagStatsController', [])
         })
 
       var g = new Graph({type: 'directed', allowSelfLoops: false})
-      g.addNodesFrom(weIndex)
-      g.importEdges(validLinks)
+
+      for (var k in weIndex)
+        g.addNode(k, weIndex[k])
+
+      validLinks.forEach(function(l) {
+        g.importEdge(l)
+      })
 
       // Default nodes appearance
       g.nodes().forEach(function(nid){
@@ -419,5 +424,5 @@ angular.module('hyphe.toolNetworkTagStatsController', [])
         $scope.loadingStatus = false
       })
     }
-  
+
   })

commit 561ad950f38ad3a9dbc884608d0608b5340c4a4e

woups looks like this view was broken since 6fa1ebc0bdae16f789665774d8100d8b606cbc5f, Boooooh @Yomguithereal

hyphe_frontend/app/views/webentity_pagesNetwork.js
@@ -78,7 +78,7 @@ angular.module('hyphe.webentityPagesNetworkController', [])
           $scope.pagesToken = result.token
 
           var percent = Math.round(100 * $scope.pages.length / $scope.webentity.pages_total)
-          $scope.status = {message: 'Loading pages ' + percent + ' %', progress: percent}            
+          $scope.status = {message: 'Loading pages ' + percent + ' %', progress: percent}
 
           if ($scope.loadAllPages && $scope.pagesToken) {
             $timeout(loadPages, 0)
@@ -105,7 +105,7 @@ angular.module('hyphe.webentityPagesNetworkController', [])
           buildNetwork(result)
         }
         ,function(){
-          $scope.status = {message: 'Error loading web entity', background: 'danger'}
+          $scope.status = {message: 'Error loading links', background: 'danger'}
         }
       )
     }
@@ -210,7 +210,7 @@ angular.module('hyphe.webentityPagesNetworkController', [])
       for (var k in nIndex)
         g.addNode(k, Object.assign({}, nIndex[k]))
 
-      validLinks.forEach(function(l) {
+      links.forEach(function(l) {
         g.importEdge(l)
       })
 