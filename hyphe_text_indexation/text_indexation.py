9de369e83f24ddb0387a7bdad1f512584c315884
Paul Girard <paul.girard@sciencespo.fr>
2020-05-07 18:15:59 +0200

    logging system + gentle SIGINT exit

--- hyphe_text_indexation/text_indexation.py
+++ hyphe_text_indexation/text_indexation.py
@@ -5,6 +5,7 @@ import time
 import zlib
 import signal
 import traceback
+import signal
 # utils
 from collections import Counter
 import datetime
@@ -14,7 +15,8 @@ from elasticsearch import Elasticsearch, helpers
 from elasticsearch_utils import connect_to_es
 # multiprocessing
 from multiprocessing import Process, Queue
-
+import logging
+import logging.handlers
 # html to text methods
 from html2text import textify
 # TODO: import only if needed by conf to avoid installing deps ? 
@@ -53,6 +55,7 @@ hyphe_corpus_coll = mongo["hyphe"]["corpus"]
 
 
 def indexation_task(corpus, es, mongo):
+    logg = logging.getLogger()
     total = 0
     jobs = set()
     pages = []
@@ -89,8 +92,9 @@ def indexation_task(corpus, es, mongo):
         try:
             page_to_index["dragnet"] = extract_content(body, encoding=encoding)
         except Exception as e:
-            print("DRAGNET ERROR")
-        page_to_index["dragnet"] = None
+            # TODO : add page id or something
+            logg.exception("DRAGNET ERROR")
+            page_to_index["dragnet"] = None
         page_to_index["indexDate"] = datetime.datetime.now()
         pages.append(page_to_index)
     # index batch to ES
@@ -103,22 +107,24 @@ def indexation_task(corpus, es, mongo):
                 for p in pages],
             index='hyphe_%s' % corpus)
         if index_result > 0:
-            print("%s: %s pages indexed"%(corpus, index_result))
+            logg.info("%s: %s pages indexed"%(corpus, index_result))
         # update status in mongo
         mongo_update = mongo_pages_coll.update_many({'url' : {'$in' : [p['url'] for p in pages]}}, {'$set': {'to_index': False}}, upsert=False)
     except Exception as e:
-        print("%s: error in index bulk"%corpus)
-        raise e
+        logg.exception("%s: error in index bulk"%corpus)
     # tag jobs when completed
     not_completed_jobs_pipeline = [
         {
-        "$match": {
-        "_job" : {"$in": list(jobs)},
-        "to_index": True
-        }},{
-        "$group": {
-            "_id": "$_job"
-        }}
+            "$match": {
+            "_job" : {"$in": list(jobs)},
+            "to_index": True
+            }
+        },
+        {
+            "$group": {
+                "_id": "$_job"
+            }
+        }
     ]
     # counting completed jobs
     not_completed_jobs = set(o['_id'] for o in mongo_pages_coll.aggregate(not_completed_jobs_pipeline))
@@ -127,14 +133,15 @@ def indexation_task(corpus, es, mongo):
         mongo_jobs_coll = mongo["hyphe_%s" % corpus]["jobs"]
         r = mongo_jobs_coll.update_many({'crawljob_id': {"$in": list(completed_jobs)}, 'crawling_status': {"$in":['FINISHED', 'CANCELED', 'RETRIED']}}, {'$set': {'text_indexed': True}})
         if r.matched_count > 0:
-            print("%s: %s of %s jobs were fully indexed"%(corpus, r.matched_count, completed_jobs))
+            logg.info("%s: %s of %s jobs were fully indexed"%(corpus, r.matched_count, completed_jobs))
 
 def updateWE_task(corpus, es, mongo):
+    logg = logging.getLogger()
     # update web entity - page structure
     mongo_webupdates_coll =  mongo["hyphe_%s" % corpus]["WEupdates"]
     mongo_jobs_coll =  mongo["hyphe_%s" % corpus]["jobs"]
     weupdates = list(mongo_webupdates_coll.find({"index_status": "PENDING"}).sort('timestamp'))
-    print("%s: %s WE updates waiting"%(corpus, len(weupdates)))
+    logg.info("%s: %s WE updates waiting"%(corpus, len(weupdates)))
     for weupdate in weupdates:
         nb_unindexed_jobs = mongo_jobs_coll.count_documents({"webentity_id": weupdate['old_webentity'], "text_indexed": {"$exists": False}, "started_at":{"$lt":weupdate['timestamp']}})
         # don't update WE structure in text index if there is one crawling job
@@ -183,124 +190,166 @@ def updateWE_task(corpus, es, mongo):
                     }
                 }
             index_result = es.update_by_query(index='hyphe_%s' % corpus, body = updateQuery, conflicts="proceed")
-            print("%s: %s pages updates"%(corpus, index_result['updated']))
+            logg.info("%s: %s pages updates"%(corpus, index_result['updated']))
             weupdates = mongo_webupdates_coll.update_one({"_id": weupdate['_id']}, {'$set': {'index_status': 'FINISHED'}})
 
 # worker
-def indexation_worker(worker_id, input):
+def indexation_worker(input, logging_queue):
+    # leave sigint handling to the parent process 
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
     es = connect_to_es(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT, ELASTICSEARCH_TIMEOUT_SEC)
     mongo = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
-    # avoid waiting for the queue to be flushed
-    input.cancel_join_thread()
-    for task in iter(input.get, 'STOP'):
-        if task['type'] == "indexation":
-            indexation_task(task['corpus'] , es, mongo)
-            
-        if task['type'] == "updateWE":
-            updateWE_task(task['corpus'], es, mongo)
-            
-    print('worker %s stopping'%worker_id)
-    exit(0)
+    #logging
+    logging_handler = logging.handlers.QueueHandler(logging_queue)
+    logg = logging.getLogger()
+    logg.handlers = []
+    logg.setLevel(logging.INFO)
+    logg.addHandler(logging_handler)
 
+
+    for task in iter(input.get, 'STOP'):
+        try:
+            if task['type'] == "indexation":
+                indexation_task(task['corpus'] , es, mongo)
+            if task['type'] == "updateWE":
+                updateWE_task(task['corpus'], es, mongo)
+        except Exception:
+            logg.exception("ERROR in task %s for corpus %s"%(task['type'],task['corpus']))       
+    logg.info('stopping')
+    input.close()
+    logging_queue.close()
+    exit
 
 
 # Create queues
 task_queue = Queue(NB_INDEXATION_WORKERS)
+# logging queue
+logging_queue = Queue(-1)  # no limit on size
 
-# start workers
-workers = []
-print("starting %s workers"%NB_INDEXATION_WORKERS)
-for i in range(NB_INDEXATION_WORKERS):
-    # create a dedicated connections to db
-   
-    p = Process(target=indexation_worker, args=(i, task_queue), daemon=True)
-    p.start()
-    workers.append(p)
+# set logging
+file_handler = logging.handlers.RotatingFileHandler('hyphe_text_indexation.log', 'a', 5242880, 4)
+console_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(processName)s %(levelname)s %(message)s')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+console_handler.setLevel(logging.DEBUG)
+logging_listener = logging.handlers.QueueListener(logging_queue, file_handler, console_handler)
 
-# TODO: INTERRUPTION HANDLING
-first_run = True
-UPDATE_WE_FREQ = 5 # unit is the number of indexation batches 
-nb_index_batches_since_last_update = Counter()
-throttle = 0.5
-while True:
-    # get and init corpus index
-    corpora = []
-    nb_pages_to_index = {}
-    nb_we_updates = {}
-    for c in hyphe_corpus_coll.find({"options.indexTextContent": True}, projection=["_id"]):
-        corpus = c["_id"]
-        mongo_pages_coll = mongo["hyphe_%s" % corpus]["pages"]
-        if first_run and RESET_MONGO:
-            mongo_pages_coll = mongo["hyphe_%s" % corpus]["pages"]
-            mongo_pages_coll.update_many({}, {'$set': {'to_index': True}}, upsert=False)
-            print('mongo index created')
-            mongo_pages_coll.update_many({'$or': [
-                {'content_type': {"$not": {"$in": ["text/plain", "text/html"]}}},
-                {'body': {'$exists': False}},
-                {'status': {"$ne": 200}},
-                {'size': 0}]},
-                {'$set': {'to_index': False}},  upsert=False)
-            mongo["hyphe_%s" % corpus]["WEupdates"].update_many({},{'$set':{'index_status': 'PENDING'}})
-            mongo["hyphe_%s" % corpus]["jobs"].update_many({},{'$unset':{'text_indexed':True}})
-        
-        nb_pages_to_index[corpus] = mongo_pages_coll.count_documents({
-            "to_index": True,
-            "forgotten": False
-        })
-        nb_we_updates[corpus] = mongo["hyphe_%s" % corpus]["WEupdates"].count_documents({"index_status": "PENDING"})
-        corpora.append(corpus)
-        # check index exists in elasticsearch
-        index_exists =  es.indices.exists(index=index_name(corpus))
-        if not index_exists or (first_run and index_exists and DELETE_INDEX):
-            if (first_run and  index_exists and DELETE_INDEX):
-                es.indices.delete(index=index_name(corpus))
-                print('index %s deleted'%corpus)
-            # create ES index
-            es.indices.create(index=index_name(corpus), body = index_mappings)        
-            print("index %s created"%corpus)
-    # order corpus by last inserts
-    lastIndexDates = {r['key']:r['maxIndexDate']['value'] for r in es.search(body={
-        "size":0,
-        "aggs": {
-            "indices": {
-            "terms": {
-                "field": "_index"   
-            },
-            "aggs":{
-                "maxIndexDate": { "max" : { "field" : "indexDate" } }
-                }
-            }  
-        }
-    })["aggregations"]["indices"]["buckets"]}
-    corpora = sorted(corpora, key=lambda c : lastIndexDates[index_name(c)] if index_name(c) in lastIndexDates else 0)
+try: 
+
+    logging_listener.start()
+    # The log output will display the thread which generated
+    # the event (the main thread) rather than the internal
+    # thread which monitors the internal queue. This is what
+    # you want to happen.
+    queue_handler = logging.handlers.QueueHandler(logging_queue)
+    logg = logging.getLogger()
+    logg.setLevel(logging.INFO)
+    logging.getLogger(name='elasticsearch').setLevel(logging.WARNING)
+    logg.addHandler(queue_handler)
+
+
+    # start workers
+    workers = []
+    logg.info("starting %s workers"%NB_INDEXATION_WORKERS)
+    for i in range(NB_INDEXATION_WORKERS):
+        # create a dedicated connections to db
     
-    # add tasks in queue
-    # TODO: check there are pages to index
-   
-   
-    for c in corpora:
-        if nb_pages_to_index[c] > 0:
-            task_queue.put({"type": "indexation", "corpus": c})
-        nb_index_batches_since_last_update[c]+=1
-    # TODO: SET a different order for updateWE ? 
-    for c in corpora:
-        if nb_we_updates[c] > 0 and nb_index_batches_since_last_update[c] > UPDATE_WE_FREQ:
-            task_queue.put({"type": "updateWE", "corpus": c})
-            nb_index_batches_since_last_update[c]=0
-    first_run = False
+        p = Process(target=indexation_worker, args=(task_queue, logging_queue), daemon=True, name="worder-%s"%i)
+        p.start()
+        workers.append(p)
 
-    # loop
-    #TODO: throttle if batch empty
-    if sum(nb_pages_to_index.values()) == 0 and sum(nb_we_updates.values()) == 0: 
-        # wait for more tasks to be created
-        print('waiting %s'%throttle)
-        time.sleep(throttle)
-        if throttle < 5:
-            throttle += 0.5
-    else:
-        # next throttlet will be 
-        throttle = 0.5
- 
-    # # Tell child processes to stop
-    # for i in range(NB_PROCESS):
-    #     task_queue.put('STOP')
+    # TODO: INTERRUPTION HANDLING
+    first_run = True
+    UPDATE_WE_FREQ = 5 # unit is the number of indexation batches 
+    nb_index_batches_since_last_update = Counter()
+    throttle = 0.5
+    while True:
+        # get and init corpus index
+        corpora = []
+        nb_pages_to_index = {}
+        nb_we_updates = {}
+        for c in hyphe_corpus_coll.find({"options.indexTextContent": True}, projection=["_id"]):
+            corpus = c["_id"]
+            mongo_pages_coll = mongo["hyphe_%s" % corpus]["pages"]
+            if first_run and RESET_MONGO:
+                logg.info("resetting mongo")
+                mongo_pages_coll = mongo["hyphe_%s" % corpus]["pages"]
+                mongo_pages_coll.update_many({}, {'$set': {'to_index': True}}, upsert=False)
+                mongo_pages_coll.update_many({'$or': [
+                    {'content_type': {"$not": {"$in": ["text/plain", "text/html"]}}},
+                    {'body': {'$exists': False}},
+                    {'status': {"$ne": 200}},
+                    {'size': 0}]},
+                    {'$set': {'to_index': False}},  upsert=False)
+                mongo["hyphe_%s" % corpus]["WEupdates"].update_many({},{'$set':{'index_status': 'PENDING'}})
+                mongo["hyphe_%s" % corpus]["jobs"].update_many({},{'$unset':{'text_indexed':True}})
+            
+            nb_pages_to_index[corpus] = mongo_pages_coll.count_documents({
+                "to_index": True,
+                "forgotten": False
+            })
+            nb_we_updates[corpus] = mongo["hyphe_%s" % corpus]["WEupdates"].count_documents({"index_status": "PENDING"})
+            corpora.append(corpus)
+            # check index exists in elasticsearch
+            index_exists =  es.indices.exists(index=index_name(corpus))
+            if not index_exists or (first_run and index_exists and DELETE_INDEX):
+                if (first_run and  index_exists and DELETE_INDEX):
+                    es.indices.delete(index=index_name(corpus))
+                    logg.info('index %s deleted'%corpus)
+                # create ES index
+                es.indices.create(index=index_name(corpus), body = index_mappings)        
+                logg.info("index %s created"%corpus)
+        # order corpus by last inserts
+        lastIndexDates = {r['key']:r['maxIndexDate']['value'] for r in es.search(body={
+            "size":0,
+            "aggs": {
+                "indices": {
+                "terms": {
+                    "field": "_index"   
+                },
+                "aggs":{
+                    "maxIndexDate": { "max" : { "field" : "indexDate" } }
+                    }
+                }  
+            }
+        })["aggregations"]["indices"]["buckets"]}
+        corpora = sorted(corpora, key=lambda c : lastIndexDates[index_name(c)] if index_name(c) in lastIndexDates else 0)
+        
+        # add tasks in queue
+        for c in corpora:
+            if nb_pages_to_index[c] > 0:
+                task_queue.put({"type": "indexation", "corpus": c})
+            nb_index_batches_since_last_update[c]+=1
+        # TODO: SET a different order for updateWE ? 
+        for c in corpora:
+            if nb_we_updates[c] > 0 and nb_index_batches_since_last_update[c] > UPDATE_WE_FREQ:
+                task_queue.put({"type": "updateWE", "corpus": c})
+                nb_index_batches_since_last_update[c]=0
+        first_run = False
+
+        # loop
+        if sum(nb_pages_to_index.values()) == 0 and sum(nb_we_updates.values()) == 0: 
+            # wait for more tasks to be created
+            logg.info('waiting %s'%throttle)
+            time.sleep(throttle)
+            if throttle < 5:
+                throttle += 0.5
+        else:
+            # next throttlet will be 
+            throttle = 0.5
+except KeyboardInterrupt:
+    logg.info('waiting for workers to stop')
+    # flush pending tasks
+    while not task_queue.empty():
+        task_queue.get_nowait()
+    # stop workers
+    for _ in range(NB_INDEXATION_WORKERS):
+        task_queue.put('STOP')
+    # wait for them to finish their current task
+    for w in workers:
+        w.join(timeout=1000)
+    task_queue.close()
+    logg.info('workers died, killing myself')
+    logging_listener.stop()
