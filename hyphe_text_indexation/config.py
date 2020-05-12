78f22afad23cdac30b75881884dee42e42c9b1b9
Benoit Simard <contact@bsimard.com>
2020-05-12 16:17:19 +0200

    Correct spelling of var HYPHE_TXT_INDEXATION_UPDATE_WE_FREQ

--- hyphe_text_indexation/config.py
+++ hyphe_text_indexation/config.py
@@ -8,7 +8,7 @@ MONGO_HOST = os.environ.get("HYPHE_MONGO_HOST","localhost")
 MONGO_PORT = int(os.environ.get("HYPHE_MONGO_PORT", 27017))
 NB_INDEXATION_WORKERS = int(os.environ.get("HYPHE_TXT_INDEXATION_NB_INDEXATION_WORKERS", 2))
 BATCH_SIZE = int(os.environ.get("HYPHE_TXT_INDEXATION_BATCH_SIZE", 10))
-UPDATE_WE_FREQ = int(os.environ.get("UHYPHE_TXT_INDEXATION_PDATE_WE_FREQ", 5))
+UPDATE_WE_FREQ = int(os.environ.get("HYPHE_TXT_INDEXATION_UPDATE_WE_FREQ", 5))
 DELETE_INDEX = os.environ.get("HYPHE_TXT_INDEXATION_DELETE_INDEX", 'false') == 'true'
 RESET_MONGO = os.environ.get("HYPHE_TXT_INDEXATION_RESET_MONGO", 'false') == 'true'
 
