a6c1be1f9e87a974697bf9d72c71fe77bad8c609
Benjamin Ooghe <benjamin.ooghe@sciencespo.fr>
2021-07-07 11:47:28 +0200

    prepare prod archives bnf (#372)

--- hyphe_backend/lib/webarchives.py
+++ hyphe_backend/lib/webarchives.py
@@ -14,9 +14,11 @@ ARCHIVES_OPTIONS = {
     "archivesinternet.bnf.fr": {
         "label": "ArchivesInternet.BNF.fr",
         "description": "crawl France's official web archives maintained by BNF",
-        "url_prefix": "http://archivesinternet.bnf.fr",
-        "proxy": "archivesinternet.bnf.fr:9115",
-        "min_date": "2007-01-01"
+        "url_prefix": "http://pfcarchivesinternet.bnf.fr",
+        "url_prefix_prod": "http://archivesinternet.bnf.fr",
+        "proxy": "pfcarchivesinternet.bnf.fr:9115",
+        "proxy_prod": "archivesinternet.bnf.fr:8090",
+        "min_date": "1996-01-01"
     }
 }
 
