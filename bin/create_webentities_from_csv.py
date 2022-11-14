183bcc4e36a23b25cb790009a50875606311d287
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2022-11-14 17:27:51 +0100

    improve corpus from csv reloader

--- bin/create_webentities_from_csv.py
+++ bin/create_webentities_from_csv.py
@@ -18,11 +18,13 @@ from progressbar import ProgressBar
 @click.argument('csv_file')
 @click.argument('corpus_id')
 @click.argument('api_url')
-@click.option('-n', '--name', default="Nom", type=str, show_default=True, help="CSV field for names")
+@click.option('-n', '--name', default="name", type=str, show_default=True, help="CSV field for names")
 @click.option('-e', '--extra_name', default=None, type=str, show_default=True, help="CSV field for extra name information")
-@click.option('-u', '--urls', default="Url", type=str, show_default=True, help="CSV fields containing urls to use as prefixes/startpages, separated by commas (the first one found will be used as homepage)")
+@click.option('-p', '--prefixes', default="prefixes", type=str, show_default=True, help="CSV field containing urls to use as prefixes, separated by pipes or spaces")
+@click.option('-h', '--homepage', default="home_page", type=str, show_default=True, help="CSV field containing url to use as homepage")
+@click.option('-s', '--startpages', default="start_pages", type=str, show_default=True, help="CSV field containing urls to use as startpages, separated by pipes or spaces")
 @click.option('-t', '--tags', default=None, type=str, show_default=True, help="CSV fields containing values desired as tags, separated by commas")
-def cli(csv_file, corpus_id, api_url, name, extra_name, urls, tags):
+def cli(csv_file, corpus_id, api_url, name, extra_name, prefixes, homepage, startpages, tags):
     try:
         hyphe_api = jsonrpclib.Server(api_url, version=1)
         print 'INFO: Connected to API at', api_url
@@ -42,7 +44,9 @@ def cli(csv_file, corpus_id, api_url, name, extra_name, urls, tags):
     name = name.encode('utf-8')
     if extra_name:
         extra_name = extra_name.encode('utf-8')
-    urls = urls.encode('utf-8').split(',')
+    prefixes = prefixes.encode('utf-8')
+    homepage = homepage.encode('utf-8')
+    startpages = startpages.encode('utf-8')
     if tags:
         tags = tags.encode('utf-8').split(',')
     else:
@@ -53,13 +57,14 @@ def cli(csv_file, corpus_id, api_url, name, extra_name, urls, tags):
         first[name]
         if extra_name:
             first[extra_name]
-        for u in urls:
-            first[u]
+        first[prefixes]
+        first[homepage]
+        first[startpages]
         for t in tags:
             first[t]
     except Exception as e:
         print >> sys.stderr, 'ERROR: some requested fields are missing in CSV file %s' % csv_file, type(e), e
-        print >> sys.stderr, 'Requested fields:', name, extra_name, urls, tags
+        print >> sys.stderr, 'Requested fields:', name, extra_name, prefixes, homepage, startpages, tags
         print >> sys.stderr, 'Available fields:', first.keys()
         return
 
@@ -102,31 +107,33 @@ def cli(csv_file, corpus_id, api_url, name, extra_name, urls, tags):
         wename = we[name]
         if extra_name and we[extra_name]:
             wename += " (%s)" % we[extra_name]
-        startpages = set([])
-        for f in urls:
-            we[f] = we[f].strip()
-            if we[f] and we[f].lower().startswith("http"):
-                startpages |= set(re.split('[ \n]+', we[f]))
-        startpages = list(startpages)
-        if not startpages:
-            print >> sys.stderr, "WARNING: no url, skipping WebEntity", wename
+
+        we[homepage] = we[homepage].strip()
+        for f in [prefixes, startpages]:
+            we[f] = list(set(re.split(r'[ \n|]+', we[f].strip())))
+        if not we[prefixes]:
+            print >> sys.stderr, "WARNING: no prefix, skipping WebEntity", wename, we
             continue
 
-        res = hyphe_api.store.declare_webentity_by_lrus_as_urls(startpages, wename, "IN", startpages, True, cid)
+        wetags = {}
+        for t in tags:
+            wetags[t] = [we[t]]
+
+        res = hyphe_api.store.declare_webentity_by_lrus_as_urls(we[prefixes], wename, "IN", we[startpages], False, {"USER": wetags}, cid)
         if 'code' not in res or res['code'] == 'fail':
             print >> sys.stderr, 'ERROR: Could not declare WebEntity', wename, startpages, res
             continue
         weid = res['result']['id']
 
-        res = hyphe_api.store.set_webentity_homepage(weid, startpages[0], cid)
+        res = hyphe_api.store.set_webentity_homepage(weid, we[homepage], cid)
         if 'code' not in res or res['code'] == 'fail':
             print >> sys.stderr, "WARNING: Could not set WebEntity's homepage", weid, wename, startpages[0], res
 
-        for t in tags:
-            if we[t]:
-                res = hyphe_api.store.add_webentity_tag_value(weid, 'USER', t, we[t].strip(), cid)
-                if 'code' not in res or res['code'] == 'fail':
-                    print >> sys.stderr, "WARNING: Could not add WebEntity's tag", weid, wename, t, we[t], res
+        #for t in tags:
+        #    if we[t]:
+        #        res = hyphe_api.store.add_webentity_tag_value(weid, 'USER', t, we[t].strip(), cid)
+        #        if 'code' not in res or res['code'] == 'fail':
+        #            print >> sys.stderr, "WARNING: Could not add WebEntity's tag", weid, wename, t, we[t], res
 
 
 if __name__ == '__main__':
